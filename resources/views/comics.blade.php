b79093da5c214d8867ebd27d3c3a344c7b94b423
andreamazza84 <andrea.mazza84@gmail.com>
2021-03-04 17:09:42 +0100

    Lavoro su header

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,18 @@
+{{-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Boolean | DC</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Amaranth:wght@400;700&family=Bebas+Neue&family=Passion+One&display=swap" rel="stylesheet">     
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <h1>Ciao</h1>
+    </body>
+</html> --}}
+@extends('layouts.app')
